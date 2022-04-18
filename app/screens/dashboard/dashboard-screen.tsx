663d882175bd5109a259c7c0861bf80d4ce8f533
Ivan Cherkasov <lijohnttle@outlook.com>
2022-04-18 23:46:42 +0200

    Add header

--- app/screens/dashboard/dashboard-screen.tsx
+++ app/screens/dashboard/dashboard-screen.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { NavigatorParamList, useBackButtonHandler } from '../../navigators';
 import { styles } from './dashboard-screen.styles';
-import { Screen } from '../../components';
+import { Header, Screen } from '../../components';
 import { color } from '../../theme';
 
 
@@ -13,7 +13,7 @@ export const DashboardScreen: FC<StackScreenProps<NavigatorParamList,'dashboard'
   return (
     <View testID="DashboardScreen" style={styles.root}>
       <Screen preset="scroll" backgroundColor={color.transparent}>
-
+        <Header headerTx="dashboardScreen.title" />
       </Screen>
     </View>
   );

ae6b60d19ede84ce90eff558034434ff0cee0443
Ivan Cherkasov <lijohnttle@outlook.com>
2022-04-18 16:25:35 +0200

    Update npm packages

--- android/app/src/main/java/com/habittracker/MainActivity.java
+++ android/app/src/main/java/com/habittracker/MainActivity.java
@@ -18,21 +18,4 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "HabitTracker";
   }
-
-  /**
-   * Required for proper react-native-gesture-handler touch handling
-   * https://docs.swmansion.com/react-native-gesture-handler/docs/#updating-mainactivityjava
-   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(
-      this,
-      new ReactActivityDelegate(this, getMainComponentName()) {
-        @Override
-        protected ReactRootView createRootView() {
-          return new RNGestureHandlerEnabledRootView(MainActivity.this);
-        }
-      }
-    );
-  }
 }
